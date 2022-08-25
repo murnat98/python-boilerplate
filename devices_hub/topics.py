96ab198b7af479d6bb246bb0f3a3336d13fae0ff
murnat98 <murnat98@yandex.ru>
2022-08-25 13:46:46 +0400

    feat(*): tasmota switch subscriber

--- devices_hub/topics.py
+++ devices_hub/topics.py
@@ -1,6 +1,7 @@
 from devices_hub.mqtt.topics import Topic
-from devices_hub.subscribers import SonoffControlSubscriber
+from devices_hub.subscribers import SonoffControlSubscriber, TasmotaSwitchSubscriber
 
 topics = [
     Topic('sonoff/switch', SonoffControlSubscriber.as_subscriber()),
+    Topic('tasmota/switch', TasmotaSwitchSubscriber.as_subscriber()),
 ]
